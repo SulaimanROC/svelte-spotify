b323847d8b72a492598e1724e5925b6757d20f46
Sulaiman Hussain <118259411+SulaimanROC@users.noreply.github.com>
2024-10-09 10:54:29 +0200

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import 'modern-normalize/modern-normalize.css';
+	import '../styles/main.scss';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+
+    $: user = data.
+    user;
+</script>
+{#if user}
+<p>Hello {user.display_name}</p>
+<form>
+    <button type="submit">Logout</button>
+</form>
+{/if}
+
+{data.user?.display_name}
+
+<slot />
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { Skull } from 'lucide-svelte';
+</script>
+
+<Skull />
+<a href="/api/auth/login">Login</a>
